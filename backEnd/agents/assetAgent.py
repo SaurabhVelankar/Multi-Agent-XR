11b0e83ef5d78fa7ce77bb90139939fc59046d37
Ray Chen <666666chenruitao@gmail.com>
2025-11-19 18:27:51 -0500

    add description for assetAgent.py

--- backEnd/agents/assetAgent.py
+++ backEnd/agents/assetAgent.py
@@ -4,4 +4,9 @@ import os
 
 class AssetAgent:
     def __init__(self):
+        """
+        Description: 
+            The Asset agent is used to handle the insertion and the deleltion of 
+            the items within in the scene according to human prompt.
+        """
         self.model = genai.GenerativeModel('gemini-2.5-flash-lite')
